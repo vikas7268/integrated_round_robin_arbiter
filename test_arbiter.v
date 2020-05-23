b0702cc1565d5a9092f0fa128c5411590592b0c8
vikas7268 <32233298+vikas7268@users.noreply.github.com>
2020-05-23 06:11:07 +0530

    Update test_arbiter.v

--- test_arbiter.v
+++ test_arbiter.v
@@ -1,21 +1,8 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.05.2020 04:08:28
+
 // Design Name: 
 // Module Name: test_arbiter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
